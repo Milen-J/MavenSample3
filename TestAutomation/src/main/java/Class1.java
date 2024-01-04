5b55c7470d429f6aad418de76a50cb8bd6b99e16
ReshmaSureshk <155421352+ReshmaSureshk@users.noreply.github.com>
2024-01-04 12:36:42 +0530

    Update Class1.java
    test

--- TestAutomation/src/main/java/Class1.java
+++ TestAutomation/src/main/java/Class1.java
@@ -12,7 +12,7 @@ public class Class1 {
 			n=n/10;
 		}
 		if (temp==sum)
-			System.out.println("palindrome number");
+			System.out.println("palinndrome number");
 		else
 			System.out.println("not palindrome");
 		}
